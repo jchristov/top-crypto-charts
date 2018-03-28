85c53a600dce649cffbbd03c4a7fb3d99a0751d2
cuppacoffee93 <cuppacoffee93@gmail.com>
2018-03-28 23:46:39 +0100

    Updated environment variables

--- js/database.js
+++ js/database.js
@@ -3,9 +3,9 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: process.env.MYSQL_SERVICE_HOST,
   port: process.env.MYSQL_SERVICE_PORT,
-  user: process.env.databaseuser,
-  password: process.env.databasepassword,
-  database: process.env.databasename
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
 
